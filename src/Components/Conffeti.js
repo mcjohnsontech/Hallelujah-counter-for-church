8bf2742e89baf4998b780dfe167c6167839bac2b
mcjohnsontech <mcjohnson144@outlook.com>
2023-12-15 07:02:19 +0100

    here made some design changes

--- src/Components/Conffeti.js
+++ src/Components/Conffeti.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { } from 'react'
 import { useParams } from 'react-router-dom'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Donfetti from 'react-confetti'
 
 function Conffeti() {
+  const { width, height } = useWindowSize()
+
+  // useEffect(() => {
+  // }, [])
+  
   let {id} = useParams();
   return (
-    <div className="App-header">
-      <p>We just completed</p>
-      <h1>{id} Hallelujah</h1>
+    
+    <div className="App-header text">
+      <Donfetti
+      width={width}
+      height={height}/>
+      <p>Praise God!</p>
+      <h1>We just completed {id} Hallelujah</h1>
     </div>
     
   )
